e5eaa86c130848d09b4c83bb19336571771b9851
Laura Carvalho <127751470+lauracar2@users.noreply.github.com>
2023-04-20 11:19:15 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$varial = 10;
+
+if ($varial < 20) {
+    echo "é menor que 20";
+
+}else {
+    echo "é maior que 20";
+}
+
+echo "<br>";
+
+for($i=0; $i<=10; $i++) {
+    echo $i."<br>";
+}
+?>
